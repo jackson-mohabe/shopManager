05398a47df8ab94d60a403a60ca00daf14aaa3a1
Jackson <mohabejackson@gmail.com>
2026-03-21 08:14:26 +0300

    v1

---
+++ main.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const { app, BrowserWindow } = require("electron");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+let mainWindow;
+let server;
+
+function startServer(callback) {
+  const expressApp = express();
+  const distPath = path.join(__dirname, "dist");
+
+  expressApp.use(express.static(distPath));
+
+  expressApp.use(function (req, res) {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+
+  server = http.createServer(expressApp);
+  server.listen(3000, "127.0.0.1", function () {
+    console.log("Server started on port 3000");
+    callback();
+  });
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    title: "Shop Manager",
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  mainWindow.loadURL("http://127.0.0.1:3000");
+
+  mainWindow.once("ready-to-show", function () {
+    mainWindow.show();
+  });
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", function () {
+  startServer(function () {
+    createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (server) {
+    server.close();
+  }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
